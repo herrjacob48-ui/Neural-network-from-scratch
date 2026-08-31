42b4e5183792bc0d9c3823668bcf1859756fd487
jacob herr <herrjacob48@gmail.com>
2026-08-30 21:51:25 -0400

    added network structure, forward pass, and cost calculation logic

--- src/layer.py
+++ src/layer.py
@@ -1,8 +1,15 @@
 import numpy as np
 class Layer:
-    def __init__(self, size):
-        self.a = np.array([])
-        self.w = []
-        self.gradients = []
+    # activation is a function object ex: reLu or sigmoid.
+    def __init__(self, n_in,n_out, activation=None):
+        self.W = np.random.randn(n_out, n_in)
+        self.b = np.random.randn(n_out)
+        self.activation = activation
+
+    # x = a(L-1) vector
+    def forward(self, x):
+        self.z = self.W @ x + self.b
+        self.a = self.activation(self.z)
+
         
             
--- src/network.py
+++ src/network.py
@@ -1,5 +1,29 @@
 import numpy as np
+from layer import Layer
 
 class Network:
-    def __init__(self):
-        pass
+
+    # pass a list like [2,8,8,1] for a 2-8-8-1 structure
+    def __init__(self, structure, activation):
+        self.layers = []
+        self.activation = activation
+        for i in range(len(structure)-1):
+            n_in = structure[i]
+            n_out = structure[i+1]
+            self.layers.append(Layer(n_in,n_out,self.activation))
+
+    def forward_pass(self, x):
+        for i in range(len(self.layers)):
+            if i == 0:
+                self.layers[i].forward(x)
+            else:
+                self.layers[i].forward(self.layers[i-1].a)
+        return self.layers[-1].a
+
+    # y is desired output
+    def calculate_cost(self, x, y):
+        self.cost = 0
+        output = self.forward_pass(x)
+        for i in range(len(output)):
+            self.cost += 0.5 * pow(output[i]- y[i], 2)
+        
